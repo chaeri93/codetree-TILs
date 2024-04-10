69a6c9883badcb03658396d21e15e9ffb37f56c8
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-04-10 21:25:24 +0900

    Title: 이상한 다트 게임, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240410/이상한 다트 게임/odd-dart-game.py
+++ 240410/이상한 다트 게임/odd-dart-game.py
@@ -4,32 +4,34 @@ n, m, q = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
 move = [list(map(int, input().split())) for _ in range(q)]
 
-def rotate(x,d,k):
+
+def rotate(x, d, k):
     if not d:
         k = n - k
-    while (x<=n):
-        tmp = board[x-1][k:] + board[x-1][:k]
-        board[x-1] = tmp
+    while (x <= n):
+        tmp = board[x - 1][k:] + board[x - 1][:k]
+        board[x - 1] = tmp
         x *= 2
 
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
 
 for x, d, k in move:
-    rotate(x,d,k)
+    rotate(x, d, k)
 
 cnt = 0
 new_board = copy.deepcopy(board)
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         for k in range(4):
             ni = i + dx[k]
             nj = j + dy[k]
             if nj < 0:
-                nj += n
-            elif nj >= n:
-                nj -= n
-            if 0<=ni<n and board[i][j] == board[ni][nj] and new_board[ni][nj]:
+                nj += m
+            elif nj >= m:
+                nj -= m
+            if 0 <= ni < n and board[i][j] == board[ni][nj] and new_board[ni][nj]:
                 new_board[ni][nj] = 0
                 if new_board[i][j]:
                     new_board[i][j] = 0
@@ -39,9 +41,9 @@ if not cnt:
     avg = 0
     for i in new_board:
         avg += sum(i)
-    avg = avg // n*n
+    avg = avg // n * m
     for i in range(n):
-        for j in range(n):
+        for j in range(m):
             if new_board[i][j] > avg:
                 new_board[i][j] -= 1
             elif new_board[i][j] < avg:
