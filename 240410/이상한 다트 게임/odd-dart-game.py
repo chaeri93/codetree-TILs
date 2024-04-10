072407c103188a9f721555730dc72b7350d6c334
Cherry <79985974+chaeri93@users.noreply.github.com>
2024-04-10 21:56:47 +0900

    Title: 이상한 다트 게임, Time: 301ms, Memory: 28MB, Status: Passed - CodeTree

--- 240410/이상한 다트 게임/odd-dart-game.py
+++ 240410/이상한 다트 게임/odd-dart-game.py
@@ -7,11 +7,12 @@ move = [list(map(int, input().split())) for _ in range(q)]
 
 def rotate(x, d, k):
     if not d:
-        k = n - k
+        k = m - k
+    cnt = x
     while (x <= n):
         tmp = board[x - 1][k:] + board[x - 1][:k]
         board[x - 1] = tmp
-        x *= 2
+        x += cnt
 
 
 dx = [-1, 0, 1, 0]
@@ -20,37 +21,43 @@ dy = [0, 1, 0, -1]
 for x, d, k in move:
     rotate(x, d, k)
 
-cnt = 0
-new_board = copy.deepcopy(board)
-for i in range(n):
-    for j in range(m):
-        for k in range(4):
-            ni = i + dx[k]
-            nj = j + dy[k]
-            if nj < 0:
-                nj += m
-            elif nj >= m:
-                nj -= m
-            if 0 <= ni < n and board[i][j] == board[ni][nj] and new_board[ni][nj]:
-                new_board[ni][nj] = 0
-                if new_board[i][j]:
-                    new_board[i][j] = 0
-                cnt += 1
-
-if not cnt:
-    avg = 0
-    for i in new_board:
-        avg += sum(i)
-    avg = avg // n * m
+    cnt = 0
+    new_board = copy.deepcopy(board)
     for i in range(n):
         for j in range(m):
-            if new_board[i][j] > avg:
-                new_board[i][j] -= 1
-            elif new_board[i][j] < avg:
-                new_board[i][j] += 1
+            for k in range(4):
+                ni = i + dx[k]
+                nj = j + dy[k]
+                if nj < 0:
+                    nj += m
+                elif nj >= m:
+                    nj -= m
+                if 0 <= ni < n and board[i][j] == board[ni][nj] and new_board[ni][nj]:
+                    new_board[ni][nj] = 0
+                    if new_board[i][j]:
+                        new_board[i][j] = 0
+                    cnt += 1
+
+    if not cnt:
+        avg = 0
+        total = 0
+        for i in range(n):
+            for j in range(m):
+                if new_board[i][j]:
+                    total += 1
+                    avg += new_board[i][j]
+        avg = avg // total
+        for i in range(n):
+            for j in range(m):
+                if new_board[i][j]:
+                    if new_board[i][j] > avg:
+                        new_board[i][j] -= 1
+                    elif new_board[i][j] < avg:
+                        new_board[i][j] += 1
+    board = new_board
 
 answer = 0
-for i in new_board:
+for i in board:
     answer += sum(i)
 
 print(answer)
